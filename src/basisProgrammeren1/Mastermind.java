ecea83ab09037b7cac35b01e651faea60868cfac
1226563 <1226563@student.roc-nijmegen.nl>
2026-09-16 11:53:18 +0200

    Added variables pins

--- src/basisProgrammeren1/Mastermind.java
+++ src/basisProgrammeren1/Mastermind.java
@@ -3,8 +3,14 @@ package basisProgrammeren1;
 public class Mastermind extends Text01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String geelPin="Geel";
+		String groenPin="Groen";
+		String blauwPin="Blauw";
+		String roodPin="Rood";
+		String paarsPin="Paars";
+		String orangePin="Oranje";
+		
 	}
 
 }
