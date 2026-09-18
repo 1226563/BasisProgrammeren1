da3b97551a2e88ef557a3752a60284ef3c4461db
1226563 <1226563@student.roc-nijmegen.nl>
2026-09-18 15:25:27 +0200

    added user input and output

--- src/basisProgrammeren1/Mastermind.java
+++ src/basisProgrammeren1/Mastermind.java
@@ -1,20 +1,31 @@
 package basisProgrammeren1;
 
-public class Mastermind extends Text01 {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		
-		String geelPin="Geel";
-		String groenPin="Groen";
-		String blauwPin="Blauw";
-		String roodPin="Rood";
-		String paarsPin="Paars";
-		String orangePin="Oranje";
-		
-		String zwartPin="Zwart";
-		String witPin="Wit";
-		String leegPin="Leeg";
-		
-	}
+public class Mastermind {
+    public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        String gok1;
+        String gok2;
+        String gok3;
+        String gok4;
+
+        System.out.println("rij 1 vakje 1:");
+        gok1 = input.nextLine();
+
+        System.out.println("rij 2 vakje 2:");
+        gok2 = input.nextLine();
+
+        System.out.println("rij 3 vakje 3:");
+        gok3 = input.nextLine();
+
+        System.out.println("rij 4 vakje 4:");
+        gok4 = input.nextLine();
+
+        System.out.println("" + gok1 + " " + gok2 + " " + gok3 + " " + gok4);
+
+        input.close();
+    }
 }
