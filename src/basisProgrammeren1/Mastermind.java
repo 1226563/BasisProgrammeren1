496e437d4643ee507de46f047b31e8861ac0958c
1226563 <1226563@student.roc-nijmegen.nl>
2026-09-18 16:12:46 +0200

    fixed java psuedocode and flowchart

--- src/basisProgrammeren1/Mastermind.java
+++ src/basisProgrammeren1/Mastermind.java
@@ -3,28 +3,52 @@ package basisProgrammeren1;
 import java.util.Scanner;
 
 public class Mastermind {
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+        String codemaker = "Speler 1";
+        String codekraker = "Speler 2";
+
+        String blauwPin = "blauw";
+        String oranjePin = "oranje";
+        String paarsPin = "paars";
+        String groenPin = "groen";
+        String roodPin = "rood";
+        String geelPin = "geel";
+
+        String codePin1 = blauwPin;
+        String codePin2 = groenPin;
+        String codePin3 = roodPin;
+        String codePin4 = geelPin;
+
+        int rij = 1;
+        boolean gewonnen = false;
 
         String gok1;
         String gok2;
         String gok3;
         String gok4;
 
-        System.out.println("rij 1 vakje 1:");
+        int zwartePinnen = 0;
+        int wittePinnen = 0;
+
+        System.out.println("Voer kleur 1 in:");
         gok1 = input.nextLine();
 
-        System.out.println("rij 2 vakje 2:");
+        System.out.println("Voer kleur 2 in:");
         gok2 = input.nextLine();
 
-        System.out.println("rij 3 vakje 3:");
+        System.out.println("Voer kleur 3 in:");
         gok3 = input.nextLine();
 
-        System.out.println("rij 4 vakje 4:");
+        System.out.println("Voer kleur 4 in:");
         gok4 = input.nextLine();
 
-        System.out.println("" + gok1 + " " + gok2 + " " + gok3 + " " + gok4);
+        System.out.println("Jouw gok is: " + gok1 + " " + gok2 + " " + gok3 + " " + gok4);
+
+        System.out.println("De code is: " + codePin1 + " " + codePin2 + " " + codePin3 + " " + codePin4);
 
         input.close();
     }
---
+++ src/basisProgrammeren1/MastermindVariables.java
@@ -0,0 +1,34 @@
+package basisProgrammeren1;
+
+public class MastermindVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String codemaker = "Speler 1";
+		String codekraker = "Speler 2";
+
+		String[] kleuren = {"blauw", "oranje", "paars", "groen", "rood", "geel"};
+
+		String codePin1;
+		String codePin2;
+		String codePin3;
+		String codePin4;
+
+		String[] code = new String[4];
+
+		int rij = 1;
+
+		boolean gewonnen = false;
+
+		String gok1;
+		String gok2;
+		String gok3;
+		String gok4;
+
+		String[] poging = new String[4];
+
+		int zwartePinnen = 0;
+		int wittePinnen = 0;
+	}
+
+}
